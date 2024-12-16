bbfaf3288f1126c48b124a3661f6190c182dad37
joao gugel <ojoaogugel@gmail.com>
2024-12-16 12:25:59 -0300

    implement a doubly linked list

---
+++ module-3/linked-lists.ts
@@ -0,0 +1,88 @@
+class LNode {
+  constructor(
+    public value: any,
+    public next: LNode | null = null,
+    public prev: LNode | null = null
+  ) {}
+}
+
+class DoublyLinkedList {
+  constructor(
+    public head: LNode | null = null,
+    public tail: LNode | null = null
+  ) {}
+
+  addToFront(value: any) {
+    const newNode = new LNode(value);
+
+    newNode.next = this.head;
+
+    if (this.head) {
+      this.head.prev = newNode;
+    }
+    if (!this.tail) {
+      this.tail = newNode;
+    }
+
+    this.head = newNode;
+  }
+
+  addToEnd(value: any) {
+    const newNode = new LNode(value);
+
+    newNode.prev = this.tail;
+
+    if (this.tail) {
+      this.tail.next = newNode;
+    } else {
+      this.head = newNode;
+    }
+
+    this.tail = newNode;
+  }
+
+  removeFromFront() {
+    if (this.head) {
+      const value = this.head.value;
+      this.head = this.head.next;
+
+      if (this.head) {
+        this.head.prev = null;
+      }
+
+      return value;
+    }
+
+    return null;
+  }
+
+  removeFromEnd() {
+    if (this.tail) {
+      const value = this.tail.value;
+
+      this.tail = this.tail.prev;
+
+      if (this.tail) {
+        this.tail.next = null;
+      }
+
+      return value;
+    }
+
+    return null;
+  }
+}
+
+const dl = new DoublyLinkedList();
+
+dl.addToEnd(10);
+dl.addToEnd(20);
+
+console.log(dl.removeFromEnd());
+console.log(dl.removeFromEnd());
+
+dl.addToFront(2);
+dl.addToFront(1);
+
+console.log(dl.removeFromEnd());
+console.log(dl.removeFromEnd());
