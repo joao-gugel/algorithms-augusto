5c5c539901fa3d314092b7ec0d98b37e669f36b2
joao gugel <ojoaogugel@gmail.com>
2024-12-16 11:51:36 -0300

    two sum with hashmaps

--- module-2/two-sum.ts
+++ module-2/two-sum.ts
@@ -8,4 +8,22 @@ function twoSum(nums: number[], target: number): number[] {
   return [0, 0];
 }
 
+// With hash maps
+function twoSum2(nums, target) {
+  let numMap = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    let complement = target - nums[i];
+
+    if (numMap[complement]) {
+      return [numMap[complement], i];
+    }
+
+    numMap[nums[i]] = i;
+  }
+
+  return [0, 0];
+}
+
 console.log(twoSum([2, 7, 11, 15], 9));
+console.log(twoSum2([2, 7, 11, 15], 9));
